3a4c3f300ceb89ea88b2be5045fc6ae3d03e4deb
pyjune <pyjune@naver.com>
2017-02-13 13:23:24 +0900

    Create 숫자카드_template.java

---
+++ 숫자카드_template.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+public class Solution {
+
+	public static void main(String[] args) throws Exception{
+		
+		System.setIn(new FileInputStream("input.txt"));
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		int [] count;
+		int num;
+		for(int tc = 1; tc<=T;tc++)
+		{
+			int N = sc.nextInt();
+			count = new int[10];
+			String card = sc.next();
+			for(int i = 0; i<N; i++)
+			{
+				num = card.charAt(i) - '0';
+				count[num]++;
+			}
+      // User Code...
+	
+		}
+	}
+
+}
